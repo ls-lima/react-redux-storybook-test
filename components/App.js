033dca3de9849da5d0a184a07a031a73cb8f5ab2
ls-lima <33849257+ls-lima@users.noreply.github.com>
2018-02-10 17:51:52 -0200

    Base project structure

---
+++ components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+               Dis my app! 
+            </div>
+        );
+    }
+}
+
+export default App;
